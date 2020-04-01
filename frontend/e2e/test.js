c9230b5dff217b22c815860d61b1ed772a09412f
Robert Wenzel <robertwenzel@potix.com>
2020-04-01 16:16:26 +0800

    cleanup/update dependencies, enable wdio tests

--- frontend/e2e/test.js
+++ frontend/e2e/test.js
@@ -2,29 +2,29 @@ const expect = require('chai').expect;
 
 describe('Shopping cart', () => {
   it('should add a product to cart and remove it', () => {
-    browser.url('https://react-shopping-cart-67954.firebaseapp.com/');
-    browser.waitForText('.shelf-item');
+    browser.url('/dev');
+
+    $('.shelf-item').waitForDisplayed();
+    $('.bag__quantity').waitForDisplayed();
 
     /* Open float cart */
-    browser.click('.bag--float-cart-closed');
+    $('.bag--float-cart-closed').click();
+    browser.pause(200); //wait for animation
 
     /* Bag should start with 0 products */
-    browser.waitForText('.bag__quantity');
-    let bagProductsQtd = browser.getText('.bag__quantity');
-    expect(bagProductsQtd).to.equal('0');
+    expect($('.bag__quantity').getText()).to.equal('0');
 
     /* Add a product to cart */
-    browser.click('.shelf-item');
-    browser.pause(100);
-
+    $('.shelf-item').click();
     /* And it should have 1 product in it now */
-    bagProductsQtd = browser.getText('.bag__quantity');
-    expect(bagProductsQtd).to.equal('1');
+    expect($('.bag__quantity').getText()).to.equal('1');
+
+    /* Add again -> 2 */
+    $('.shelf-item').click();
+    expect($('.bag__quantity').getText()).to.equal('2');
 
     /* Remove the product from cart and now it should show 0 products in bag */
-    browser.click('.shelf-item__del');
-    browser.pause(100);
-    bagProductsQtd = browser.getText('.bag__quantity');
-    expect(bagProductsQtd).to.equal('0');
+    $('.shelf-item__del').click();
+    expect($('.bag__quantity').getText()).to.equal('0');
   });
 });
